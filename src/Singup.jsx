1f2aa800c0c2fd70fd197a6f0814307ae0c75da6
Gaga Kikvilashvili <gkikvilashviliexpogeorgia.ge@Gagas-MacBook-Air.local>
2022-04-23 17:45:23 +0400

    refresh

--- src/Singup.jsx
+++ src/Singup.jsx
@@ -4,12 +4,16 @@ import React from "react";
 
 export default function Singup() {
     return (
-<div className="singup" id="singup">
-        <div className="left">
-        <div className="title">
+        <div className="singup-bg">
+            <div className="singup-header">
+            <div className="title">
             <a href="#">Sing In</a>
             <a href="#">Sing Up</a>
         </div>
+        <img src="../img/Vector.png" className="vector" alt="img" />
+            </div>
+<div className="singup" id="singup">
+        <div className="left">
         <div className="form">
         <div className="form-input">
         <label htmlFor="firstname">First name*</label>
@@ -34,8 +38,18 @@ export default function Singup() {
         </div>
         <button className="btn-signup" id="btn-signup">Sign Up</button>
         </div>
-        
-    <div className="right"></div>
+    <div className="right">
+    
+    <div className="form">
+        <p>or Sign Up via</p>
+        <button className="btn-soc">Facebook</button>
+        <button className="btn-soc">Google</button>
+        <p>by clicking submit you agree to
+the Terms and Conditions</p>
+<img src="../img/Group3.png" alt="img" />
+    </div>
+    </div>
+</div>
 </div>
     )
 }
